38a0205e56a4fc1c0e2b7822292f0441bdad40c2
David Manuel <davidmanuel.dev@gmail.com>
2023-09-04 10:51:03 -0400

    algunas cosas corregidas y diagramas subidos

--- ejercicio9/Program.cs
+++ ejercicio9/Program.cs
@@ -1,19 +1,47 @@
-﻿using System;
+﻿// using System;
 
-class Program
-{
-    static void Main()
-    {
-        Console.Write("Ingrese un número: ");
-        int n = int.Parse(Console.ReadLine());
+// class Program
+// {
+//     static void Main()
+//     {
+//         Console.Write("Ingrese un número: ");
+//         int n = int.Parse(Console.ReadLine());
 
-        for (int i = 1; i <= n; i++)
-        {
-            for (int j = 1; j <= i; j++)
-            {
-                Console.Write(i);
-            }
-            Console.WriteLine();
-        }
-    }
+//         for (int i = 1; i <= n; i++)
+//         {
+//             for (int j = 1; j <= i; j++)
+//             {
+//                 Console.Write(i);
+//             }
+//             Console.WriteLine();
+//         }
+//     }
+// }
+
+using System;
+
+namespace continua {
+	class ejercicio9 {
+
+		static void Main(string[] args) {
+			double a;
+			double b;
+			double c;
+			double n;
+			Console.Write("Escribe un numero: ");
+			n = Double.Parse(Console.ReadLine());
+			for (a=n; a>=1; --a) {
+				for (b=1; b<=a; ++b) {
+					Console.Write(" ");
+				}
+				for (c=a; c<=n; ++c) {
+					Console.Write(n-a+1+" ");
+				}
+				Console.WriteLine("");
+			}
+		}
+
+	}
+
 }
+
